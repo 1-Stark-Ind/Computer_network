51f8a2c03b6f3381df26d490cb2041636c2aba73
1-Stark-Ind <68730622+1-Stark-Ind@users.noreply.github.com>
2020-11-30 16:02:20 +0530

    Update checksum.c

--- checksum.c
+++ checksum.c
@@ -5,37 +5,68 @@
 int
 main ()
 {
-  int i = 0, sc, m, k,t;
-  char *data_steam, *temp,carry='0';
+  int i = 0, sc, m, k, t;
+  char *data_steam, *temp, carry = '0';
   sc = 8;
   m = sc;
   data_steam = malloc (40 * sizeof (char));
   temp = malloc (8 * sizeof (char));
   printf ("\nEnter 32 bit stream : ");
   scanf ("%s", &data_steam);
-  for (int r = sc-1; r >=0; r--)
+  for (int r = sc - 1; r >= 0; r--)
     {
-      strcpy (temp[sc-r-1], data_steam[r]);
+      strcpy (temp[sc - r], data_steam[r]);
     }
   while (i < 32)
     {
       i = 0;
       k = m;
       t = m + sc;
-      while (t> k )
+      while (t > k)
 	{
-	  if (strcmp (temp[i], data_steam[t]) == 0 && carry=='0')
+	  if (strcmp (temp[i], data_steam[t]) == 0 && carry == '0')
 	    {
-            if(temp[i]=='1')
-            {   
-                carry='1';
-            }
+	      if (temp[i] == '1')
+		{
+		  temp[i] = '0';
+		  carry = '1';
+		}
+	      else
+		{
+		  temp[i] = '0';
+		  carry = '1';
+		}
 	    }
+	  else if (strcmp (temp[i], data_steam[t]) == 0 && carry == '1')
+	    {
+	      if (temp[i] == '1')
+		{
+		  temp[i] = '1';
+		  carry = '1';
+		}
+	      else
+		{
+		  temp[i] = '1';
+		  carry = '0';
+		}
+	    }
+	  else if(strcmp(temp[i],data_steam[t])==1 && carry=='1')
+	  {
+	      temp[i]='0';
+	      carry='1';
+	  }
+	  else if (strcmp(temp[i],data_steam[t]==1 && carry=='0'))
+	  {
+	      temp[i]='1';
+	      carry='0';
+	  }
+	  i++;
+	  t--;
 	}
       m += sc;
       i += k;
       printf ("\n\n");
     }
-
+    printf("string is : : %s ",temp);
   return 0;
 }
