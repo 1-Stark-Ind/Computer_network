2f4dab71cf6fab8b26612c04b931f62e94749d75
1-Stark-Ind <68730622+1-Stark-Ind@users.noreply.github.com>
2020-12-01 16:36:58 +0530

    Update checksum.c

--- checksum.c
+++ checksum.c
@@ -1,72 +1,88 @@
-#include<string.h>
-#include<stdlib.h>
+#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 
-int
-main ()
+int main ()
 {
-  int i = 0, sc, m, k, t;
-  char *data_steam, *temp, carry = '0';
+  int i = 0, sc, m, k=7, t;
+  char data_stream[40], *temp,*temp1, carry = '0';
   sc = 8;
   m = sc;
-  data_steam = malloc (40 * sizeof (char));
-  temp = malloc (8 * sizeof (char));
+  temp=(char*)calloc(8,sizeof(char));
+  temp1=(char*)calloc(8,sizeof(char));
   printf ("\nEnter 32 bit stream : ");
-  scanf ("%s", &data_steam);
-  for (int r = sc - 1; r >= 0; r--)
-    {
-      strcpy (temp[sc - r], data_steam[r]);
+  scanf("%s",data_stream);
+  for (int i = 0; i <8; i++)
+    {  
+        temp[k] = *(i+data_stream);
+        k--;
     }
+
   while (i < 32)
     {
-      i = 0;
-      k = m;
-      t = m + sc;
-      while (t > k)
-	{
-	  if (strcmp (temp[i], data_steam[t]) == 0 && carry == '0')
-	    {
-	      if (temp[i] == '1')
-		{
-		  temp[i] = '0';
-		  carry = '1';
-		}
-	      else
-		{
-		  temp[i] = '0';
-		  carry = '1';
-		}
-	    }
-	  else if (strcmp (temp[i], data_steam[t]) == 0 && carry == '1')
-	    {
-	      if (temp[i] == '1')
-		{
-		  temp[i] = '1';
-		  carry = '1';
-		}
-	      else
-		{
-		  temp[i] = '1';
-		  carry = '0';
-		}
-	    }
-	  else if(strcmp(temp[i],data_steam[t])==1 && carry=='1')
-	  {
-	      temp[i]='0';
-	      carry='1';
-	  }
-	  else if (strcmp(temp[i],data_steam[t]==1 && carry=='0'))
-	  {
-	      temp[i]='1';
-	      carry='0';
-	  }
-	  i++;
-	  t--;
-	}
-      m += sc;
-      i += k;
-      printf ("\n\n");
+     i = 0;
+     k = m;
+     t = m + sc;
+     while (t > k)
+     {
+     if ((temp[i]== *(data_stream+(t-1))) && (carry == '0'))
+     {
+     if (temp[i] == '1')
+     {
+     temp[i] = '0';
+     carry = '1';
+     }
+     else
+     {
+     temp[i] = '0';
+     carry = '0';
+     }
+     }
+     else if ((temp[i]== *(data_stream+(t-1))) && (carry == '1'))
+     {
+     if (temp[i] == '1')
+     {
+     temp[i] = '1';
+     carry = '1';
+     }
+     else
+     {
+     temp[i] = '1';
+     carry = '0';
+     }
+     }
+     else if ((temp[i]!= *(data_stream+(t-1))) && (carry == '0'))
+     {
+     temp[i] = '1';
+     carry = '0';
+     }
+     else if ((temp[i]!= *(data_stream+(t-1))) && (carry == '1'))
+     {
+     temp[i] = '0';
+     carry = '1';
+     }
+     i++;
+     t--;
+     }
+     m += sc;
+     i += k;
+     
+    } 
+    
+    int b=7;
+    for(int i=0;i<8;i++)
+    {
+       if (temp[b]=='1')
+       {
+           temp1[i]='0';
+       }
+       else if(temp[b]=='0')
+       {
+           temp1[i]='1';
+       }
+       b--;
+       
     }
-    printf("string is : : %s ",temp);
-  return 0;
+    printf("\nChecksum is : : %s",temp1);
+    return 0;
 }
